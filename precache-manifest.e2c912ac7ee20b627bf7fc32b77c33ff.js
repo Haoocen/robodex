39d17dcd8c2a6dca1985b6ac9b4b281a7bdf8df1
Haocen Jiang <haocen9902@gmail.com>
2019-12-25 20:06:27 -0500

    Updates

---
+++ precache-manifest.e2c912ac7ee20b627bf7fc32b77c33ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcec81327dc18ccf8c1286bc93e65139",
+    "url": "/robodex/index.html"
+  },
+  {
+    "revision": "0c175d971d8b79635f4e",
+    "url": "/robodex/static/css/main.1d13fd97.chunk.css"
+  },
+  {
+    "revision": "76811279f48b062b11d7",
+    "url": "/robodex/static/js/2.17e948aa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robodex/static/js/2.17e948aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c175d971d8b79635f4e",
+    "url": "/robodex/static/js/main.980f7221.chunk.js"
+  },
+  {
+    "revision": "a980278c1514bba6e796",
+    "url": "/robodex/static/js/runtime-main.a757537d.js"
+  }
+]);
